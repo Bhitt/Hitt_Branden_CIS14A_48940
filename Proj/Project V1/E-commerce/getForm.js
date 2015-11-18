5cff6c9567d8fc144f774e9cfdfe94d613b0e70d
bhitt <bhitt@student.rcc.edu>
2015-11-18 10:58:04 -0800

    Signed-off-by: bhitt <bhitt@student.rcc.edu>

--- Proj/Project V1/E-commerce/getForm.js
+++ Proj/Project V1/E-commerce/getForm.js
@@ -1,24 +1,32 @@
-// JavaScript Document
-/* 
- * Branden Hitt
- * November 16th, 2015
- * 
- 	var url=document.location.href;
-	$_GET=getForm(url);
- */
 //Function which retrieves the information
 //in a form
 function getForm(url){
-        var $_GET = new Object;
 	var info=url.split("?");
-        if(info.length>1){
-            var nameValuePairs=info[1].split("&");
-            for(var i=0;i<nameValuePairs.length;i++){
-                    var map=nameValuePairs[i].split("=");
-                    var name=map[0];
-                    var value=map[1];
-                    $_GET[name]=value;
-            }
-        }
+	var nameValuePairs=info[1].split("&");
+	var $_GET = new Object;
+	for(var i=0;i<nameValuePairs.length-1;i++){
+		var map=nameValuePairs[i].split("=");
+		var name=map[0];
+		var value=map[1];
+		$_GET[name]=value;
+	}
 	return $_GET;
+}
+
+function setCookie(cname,cvalue,exdays){
+	var d=new Date9();
+	d.setTime(d.getTime() + (exdays*24*60*60*1000));
+	var expires = "expires="+d.toUTCString();
+	document.cookie=cname+"=" +cvalue +"; "+expires;
+}
+
+function getCookie(cname){
+	var name=cname+"=";
+	var ca = document.cookie.split(";");
+	for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
 }
