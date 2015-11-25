9a942b4826f5a069add2a765e05717aa99b2715f
bhitt <bhitt@student.rcc.edu>
2015-11-25 12:08:47 -0800

    Signed-off-by: bhitt <bhitt@student.rcc.edu>

--- Proj/Project V3/Sudoku/functions/puzzle.js
+++ Proj/Project V3/Sudoku/functions/puzzle.js
@@ -1,34 +1,6 @@
 // JavaScript Document
-/*display: function(location){
-	var cell=document.getElementById(location);
-}
 
-check: function(guess,guessLoc){
-	for(var x=0;x<9;x++){
-		for(var y=0;y<9;y++){
-			if(guess===key(guessLoc){
-				return 0;		
-			}else{
-				return 1;
-			}
-		}
-	}
-		
-}*/
-
-
-
-function enter(location){
-	var re = /[1-9]/; 
-	var key1 = [[4,3,5,2,6,9,7,8,1],
-				[6,8,2,5,7,1,4,9,3],
-				[1,9,7,8,3,4,5,6,2],
-				[8,2,6,1,9,5,3,4,7],
-				[3,7,4,6,8,2,9,1,5],
-				[9,5,1,7,4,3,6,2,8],
-				[5,1,9,3,2,6,8,7,4],
-				[2,4,8,9,5,7,1,3,6],
-				[7,6,3,4,1,8,2,5,9]];
+/*function enter(location){
 	
 	var loc=document.getElementById(location);
 	var box=prompt("Input Number:");
@@ -40,8 +12,39 @@ function enter(location){
 			loc.style.color="#ff0000";
 		}
 	}
+}*/
+function checkTest(){
+	var val=document.getElementById("00").value;
+	if(val==5){
+		val.value="X";
+		val.style.color="ff0000";
+	}else{
+		val.value="Y";
+		val.stlye.color="00ff00";
+	}
 }
 
 function check(){
-	console.log("This is a test");	
+	var re = /[1-9]/; 
+	var key1 = [[4,3,5,2,6,9,7,8,1],
+				[6,8,2,5,7,1,4,9,3],
+				[1,9,7,8,3,4,5,6,2],
+				[8,2,6,1,9,5,3,4,7],
+				[3,7,4,6,8,2,9,1,5],
+				[9,5,1,7,4,3,6,2,8],
+				[5,1,9,3,2,6,8,7,4],
+				[2,4,8,9,5,7,1,3,6],
+				[7,6,3,4,1,8,2,5,9]];
+				
+	for(var x=0;x<9;x++){
+		for(var y=0;y<9;y++){
+			var val=document.getElementById("x"+"y").value;
+			var key=key1[x][y];
+			if(val==key){
+				/* Do Nothing */	
+			}else{
+				val="X";	
+			}
+		}
+	}
 }
