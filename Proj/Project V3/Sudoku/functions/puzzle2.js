a2174812164156e716088b15b7c8ca43c6001a6d
bhitt <bhitt@student.rcc.edu>
2015-11-29 04:23:49 -0800

    Signed-off-by: bhitt <bhitt@student.rcc.edu>

--- Proj/Project V3/Sudoku/functions/puzzle2.js
+++ Proj/Project V3/Sudoku/functions/puzzle2.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 var mistakes=0;
 
-function checkTest(){
+/*function checkTest(){
 	var val=document.getElementById("00");
 	if(val.value == 5){
 		val.value="X";
@@ -10,20 +10,20 @@ function checkTest(){
 		val.value="Y";
 		val.style.color="#00ff00";
 	}
-}
+}*/
 
 function check(){
 	var re = /[1-9]/;
 	var count=0; 
-	var key1 = [[4,3,5,2,6,9,7,8,1],
-				[6,8,2,5,7,1,4,9,3],
-				[1,9,7,8,3,4,5,6,2],
-				[8,2,6,1,9,5,3,4,7],
-				[3,7,4,6,8,2,9,1,5],
-				[9,5,1,7,4,3,6,2,8],
-				[5,1,9,3,2,6,8,7,4],
-				[2,4,8,9,5,7,1,3,6],
-				[7,6,3,4,1,8,2,5,9]];
+	var key1 = [[4,2,7,9,3,1,5,6,8],
+				[8,9,5,7,4,6,2,3,1],
+				[6,3,1,8,2,5,9,4,7],
+				[5,4,8,2,1,7,3,9,6],
+				[9,1,3,4,6,8,7,5,2],
+				[7,6,2,5,9,3,8,1,4],
+				[2,5,9,6,7,4,1,8,3],
+				[1,8,4,3,5,2,6,7,9],
+				[3,7,6,1,8,9,4,2,5]];
 				
 	for(var x=0;x<9;x++){
 		for(var y=0;y<9;y++){
@@ -49,7 +49,7 @@ function check(){
 		location.assign("WinPage.html");
 	}
 	var correct=document.getElementById("Correct");
-	correct.value="Correct Numbers : "+(count-36);
+	correct.value="Correct Numbers : "+(count-29);
 	var errors=document.getElementById("Mistakes");
 	errors.value="Mistakes : "+mistakes;
 	var rem=document.getElementById("Remaining");
