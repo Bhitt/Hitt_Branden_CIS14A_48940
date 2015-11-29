7876c3517f9f04fc50a90db3d3ee88f3785dc83b
bhitt <bhitt@student.rcc.edu>
2015-11-28 19:56:29 -0800

    Signed-off-by: bhitt <bhitt@student.rcc.edu>

--- Proj/Project V3/Sudoku/functions/puzzle.js
+++ Proj/Project V3/Sudoku/functions/puzzle.js
@@ -15,17 +15,18 @@
 }*/
 function checkTest(){
 	var val=document.getElementById("00");
-	if(val.value==5){
+	if(val.value == 5){
 		val.value="X";
-		val.style.color="ff0000";
+		val.style.color="#ff0000";
 	}else{
 		val.value="Y";
-		val.style.color="00ff00";
+		val.style.color="#00ff00";
 	}
 }
 
 function check(){
-	var re = /[1-9]/; 
+	var re = /[1-9]/;
+	var count=0; 
 	var key1 = [[4,3,5,2,6,9,7,8,1],
 				[6,8,2,5,7,1,4,9,3],
 				[1,9,7,8,3,4,5,6,2],
@@ -43,12 +44,17 @@ function check(){
 			loc=loc+loc2;
 			var val=document.getElementById(loc);
 			var key=key1[x][y];
-			if(val.value==key|| val.value==""){
-				/* Do Nothing */	
+			if(val.value == key || val.value==""){
+				/* Increment count for win */
+				count++;	
 			}else{
 				val.value="X";
-				val.style.color="#ff0000";	
+				/*val.style.color="#ff0000";*/	
 			}
 		}
+	}
+	
+	if(count>= 45){
+		location.assign("WinPage.html");
 	}
 }
